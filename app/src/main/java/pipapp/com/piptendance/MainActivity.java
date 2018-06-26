5e9791639176c74c765d0d923f7bfe4e1de0af29
Philip Sultan <philip.sultan@programmer.net>
2018-06-26 08:45:05 +0700

    Initial commit

---
+++ app/src/main/java/pipapp/com/piptendance/MainActivity.java
@@ -0,0 +1,13 @@
+package pipapp.com.piptendance;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
